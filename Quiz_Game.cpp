1c0386f5571229c569342749b6f2f836ef2f34c1
all4nitrous <all4nitrous@gmail.com>
2025-02-09 23:39:21 -0500

    Close to finished

--- Quiz_Game.cpp
+++ Quiz_Game.cpp
@@ -13,8 +13,8 @@ int main()
                               "4. Is the Earth flat?"};
 
     std::string options[][4] = {{"A. 1969", "B. 1975", "C. 1985", "D. 1989"},
-                               {"A. Guido Van Rossum", "B. Bjarne Stroustrup", "John Carmack", "Mark Zuckerburg"},
-                               {"A. C", "B. C+", "C--", "B++"},
+                               {"A. Guido Van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerburg"},
+                               {"A. C", "B. C+", "C. C--", "D. B++"},
                                {"A. Yes", "B. No", "Sometimes", "What's Earth?"}};
     
     char answerKey[] = {'C', 'B', 'A', 'B'};
@@ -28,17 +28,36 @@ int main()
         cout << "**************************" << endl;
         cout << questions[i] << endl;
 
-        for(int j; j < sizeof(options[i])/sizeof(options[i][0]); j++) 
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++) 
         {
             cout << options[i][j] << endl;
+            
         }
         
+        cout << "Enter your answer here: ";
         cin >> guess;
         guess = toupper(guess);
+
+        if(guess == answerKey[i]) 
+        {
+            cout << "CORRECT" << endl;
+            score++;
+        }
+        else
+        {
+            cout << "WRONG!" << endl;
+            cout << "Answer: " << answerKey[i] << endl;
+        }
     }
+    cout << "**************************" << endl;
+    cout << "*        RESULTS         *" << endl;
+    cout << "**************************" << endl;
+    cout << "CORRECT GUESSES: " << score << endl;
+    cout << "# of QUESTIONS: " << size << endl;
+    cout << "SCORE: " << (score/(double)size)*100 << "%" << endl;
 
     return 0;
-}
+}  
 
 
 
